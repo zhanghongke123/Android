a8c7ead0cc6cde6f2395ed11dbab09a44a298bc7
zhanghongke123 <1432461572@qq.com>
2016-01-20 10:06:03 +0800

    添加一个按钮

--- MyProject/app/src/main/java/com/example/zhanghongke/myproject/MainActivity.java
+++ MyProject/app/src/main/java/com/example/zhanghongke/myproject/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.zhanghongke.myproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
